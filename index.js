03a3aa676a6ccbb796ab7f1823b8630e5f5fe5dd
Timothy Isidro <timothyjohn.isidro.iics@ust.edu.ph>
2020-02-07 15:20:09 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/*function sayWelcome(){
+  alert("Welcome to 2ITH");
+}
+
+
+
+let sayWelcome = function(){
+    alert("Welcome to 2ITH");
+}
+
+let sayWelcome = () => alert("Welcome to 2ITH");
+
+sayWelcome();*/
+
+/*var a = 6; // global declaration
+function one(){
+
+    var a = 5; // declaration for this function only
+}
+function two(){
+    var a = 7
+}
+
+alert (a);*/
+
+function displayPara1(){
+    let txt = document.getElementById("first").innerHTML;
+    alert(txt);
+}
